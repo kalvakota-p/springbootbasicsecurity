34a58272a72aeae30674db9b2cbecc31893d166b
Phani Charan <phanicharan.21@gmail.com>
2024-10-24 01:18:17 -0400

    first commit

---
+++ src/main/java/dev/phani/springbootbasicsecurity/SpringbootbasicsecurityApplication.java
@@ -0,0 +1,13 @@
+package dev.phani.springbootbasicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbasicsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootbasicsecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/phani/springbootbasicsecurity/SpringbootbasicsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.phani.springbootbasicsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbasicsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
